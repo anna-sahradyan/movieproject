4c73a539cbeb5e3c0de194090fc1980bdf579dc8
anna <a.sahradyan72@gmail.com>
2022-02-02 13:21:24 -0800

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from "./components/Home";
+import {Route, Routes} from "react-router-dom";
+import MovieDetails from "./components/MovieDetails";
+import Footer from "./components/Footer";
+import ErrorPage from "./components/Error";
+import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+
+        <>
+            <div className={'wrapper'}>
+            <Header/>
+                <div className={'main'}>
+            <Routes>
+                <Route exact path='/' element={<Home/>}/>
+                <Route path={'/movie/:imdbID'} element={<MovieDetails/>}/>
+                <Route path={'*'} element={<ErrorPage/>}/>
+            </Routes>
+                </div>
+                <Footer/>
+        </div>
+
+        </>
+    );
+};
 
 export default App;
